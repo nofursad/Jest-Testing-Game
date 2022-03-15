41c43dc4e51b40367bcd5d15491d41afcc6fdf22
Abhisek Joshi <nofursad@gmail.com>
2022-03-15 21:41:23 +0000

    Add test for addTurn function

--- scripts/test/game.test.js
+++ scripts/test/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const {game, newGame, showScore} = require("../game")
+const {game, newGame, showScore, addTurn} = require("../game")
 
 beforeAll(() => {
     let fs = require("fs");
@@ -44,8 +44,8 @@ describe("newGame works correctly", () => {
     test("should clear player moves array", () => {
         expect(game.playerMoves.length).toBe(0);
     });
-    test("should clear current game array", () => {
-        expect(game.currentGame.length).toBe(0);
+    test("should be one move in the computer's game array", () => {
+        expect(game.currentGame.length).toBe(1);
     });
     test("should display 0 for the element with id of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
